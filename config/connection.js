6fcb2ee591f8a5336c89817cb604a4d7b28be7b6
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-05-09 10:38:03 -0700

    another one

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "test",
-  database: "icecream_db",
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    ;
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "test",
+        database: "icecream_db",
+    });
+};
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
