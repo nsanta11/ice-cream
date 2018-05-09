6d605b48d708581f960e45fd2e8a6e588820552e
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-05-09 10:04:35 -0700

    a

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root",
   password: "test",
   database: "icecream_db",
